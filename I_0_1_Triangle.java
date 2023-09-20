dbbea1407d538fdeef92bf3f6ea6e560a2fbd8a4
Prathmesh.Gorle <prathmeshgorle4@gmail.com>
2023-09-21 04:39:52 +0530

    0's And 1's Triangle Added

---
+++ I_0_1_Triangle.java
@@ -0,0 +1,16 @@
+public class I_0_1_Triangle {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                int a = i + j;
+                if (a % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
