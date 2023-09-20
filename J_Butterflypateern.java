0e9a5c92532e95fdcc1015c9c8fe2d0639da761e
Prathmesh.Gorle <prathmeshgorle4@gmail.com>
2023-09-21 04:43:06 +0530

    Butterfly Advanced Pattern Added

---
+++ J_Butterflypateern.java
@@ -0,0 +1,32 @@
+public class J_Butterflypateern {
+    public static void main(String[] args) {
+        // upper half
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (5 - i); j++) { // for rotating 180 spaces 2(n-1)/
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // lower half
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (5 - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
