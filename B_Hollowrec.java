cf30c4ac2d355cca7c675459a3b7fec5872f299c
Prathmesh.Gorle <prathmeshgorle4@gmail.com>
2023-09-21 03:56:07 +0530

    new Patterns Added

---
+++ B_Hollowrec.java
@@ -0,0 +1,14 @@
+public class B_Hollowrec {
+public static void main(String[] args) {
+	for(int i=1;i<=4;i++) {
+		for(int j=1;j<=4;j++) {
+			if(i==1 ||i==4|| j==1||j==4) {
+			System.out.print("*");
+			}else {
+				System.out.print(" ");   //for spacing
+			}
+		}
+		System.out.println();
+	}
+}
+}
