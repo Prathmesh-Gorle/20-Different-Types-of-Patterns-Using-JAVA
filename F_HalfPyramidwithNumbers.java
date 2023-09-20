ba6fab9cee2bff6c55d47361a69c60170967aed1
Prathmesh.Gorle <prathmeshgorle4@gmail.com>
2023-09-21 04:08:56 +0530

    Half Pyramid with Numbers

---
+++ F_HalfPyramidwithNumbers.java
@@ -0,0 +1,11 @@
+public class F_HalfPyramidwithNumbers {
+           public static void main(String[] args) {
+			for(int i=1;i<=5;i++) {
+				for(int j=1;j<=i;j++){
+					System.out.print(j);  //only j printed instead of *
+				}
+				System.out.println();
+			}
+			
+		}
+}
