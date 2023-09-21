46951e0b4aa8c7a319e4f005d744eac4f4b92798
Prathmesh.Gorle <prathmeshgorle4@gmail.com>
2023-09-21 19:19:06 +0530

    Pyramid with same number in Row

---
+++ N_PyramidwithSameNumberinRow.java
@@ -0,0 +1,15 @@
+public class N_PyramidwithSameNumberinRow {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " "); // here we take i hence same number is printed in row
+            }
+            System.out.println();
+
+        }
+    }
+
+}
