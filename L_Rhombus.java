41e2019a8f62149e4a87ee4f03087328e36ff60a
Prathmesh.Gorle <prathmeshgorle4@gmail.com>
2023-09-21 19:12:45 +0530

    Rhombus Shape Pattern

---
+++ L_Rhombus.java
@@ -0,0 +1,16 @@
+public class L_Rhombus {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 5; j++) { // same as rectangle
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
