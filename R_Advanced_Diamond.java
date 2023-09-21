3fb034a57cc029012893533ed6296ad85e3e116a
Prathmesh.Gorle <prathmeshgorle4@gmail.com>
2023-09-21 19:47:52 +0530

    Advanced Diamond Pattern

---
+++ R_Advanced_Diamond.java
@@ -0,0 +1,25 @@
+public class R_Advanced_Diamond {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5 - i; j++) { // (n-i) spaces required
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*" + " "); // after star one space is required
+            }
+            System.out.println();
+        }
+
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= 5 - i; j++) { // (n-i) spaces required
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*" + " "); // after star one space is required
+            }
+            System.out.println();
+        }
+
+    }
+
+}
