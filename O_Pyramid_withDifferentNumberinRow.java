65ff1508ecd64bd01ae8238e62753d3d0331296d
Prathmesh.Gorle <prathmeshgorle4@gmail.com>
2023-09-21 19:22:20 +0530

    Pyramid with Different Number in Row

---
+++ O_Pyramid_withDifferentNumberinRow.java
@@ -0,0 +1,15 @@
+public class O_Pyramid_withDifferentNumberinRow {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " "); // here we take i hence same number is printed in row
+            }
+            System.out.println();
+
+        }
+    }
+
+}
